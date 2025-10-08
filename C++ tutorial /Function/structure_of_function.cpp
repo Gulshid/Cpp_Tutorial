82cdad6e6cbea9c11f5b4fd95b809f53a916c401
Gulshid Zada <gulshidzada4@gmail.com>
2025-10-08 19:03:49 +0500

    Function of C++ done

---
+++ C++ tutorial /Function/structure_of_function.cpp
@@ -0,0 +1,43 @@
+/*          
+
+         ****** function in C++ *******
+            1. function Declaration
+            2. function Definition
+            3. function Call
+
+
+            <*** function Decalaration ***>
+            int fun(int a, int b)
+            {
+                <*** function Definition ***>
+                int sum = a+b;  
+                return sum;
+
+            }
+
+
+            int main()
+            {
+                <*** function call ***>
+                add(1,2)
+            }
+*/
+
+
+#include<iostream>
+using namespace std;
+
+// function 
+void  greet()
+{
+    cout<<"This is function of C++ Programming."<<endl;
+
+}
+
+
+int main()
+{
+    greet();
+
+}
+
